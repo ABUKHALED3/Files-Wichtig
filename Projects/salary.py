ba1df660b143aede4bb1f4e7f2f07b9c64227fe0
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-05-05 21:49:07 +0300

    test ohter

---
+++ Projects/salary.py
@@ -0,0 +1,8 @@
+# ask about number of hours
+hours = input("Enter your hours: ")
+# ask about rate
+rate = input("Enter your rate: ")
+# calculate salary 
+salary = float(hours) * float(rate)
+# print salary
+print(f"Your Salary is {salary:.1f} Pounds")
