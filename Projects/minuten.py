0e32d33aa8ac417f7d08320a58bfe860d1138a98
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-05-05 21:01:57 +0300

    project to Deutschland

---
+++ Projects/minuten.py
@@ -0,0 +1,8 @@
+# ask minuten you studied
+minuten = float(input("How many minuten to studied: "))
+
+# convert minuten from Uhrs
+uhrs = minuten / 60 
+
+# print Uhrs
+print(f"Your studied {uhrs:.1f} uhrs")
