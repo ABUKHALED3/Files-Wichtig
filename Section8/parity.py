a63d477d3aba38e056b5455573a8e5ecd924ad47
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-05-12 20:11:18 +0300

    odd und even numbers

---
+++ Section8/parity.py
@@ -0,0 +1,9 @@
+# take from user number
+num = float(input("Enter Number: "))
+
+# if num % 2 == 0 
+# Ture هل الرقم الذي أدخله المستخدم باقي القسمة يساوي صفر  
+if num % 2 == 0:
+    print("the num is Even, الرقم زوجي ")
+else:
+    print("The num is Odd , الرقم فردي ")
