b3c1337e152ef4438cf7bcda2618fc5a68c0a22a
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-05-17 16:00:24 +0300

    lesson 5

---
+++ Section8/lesson5.py
@@ -0,0 +1,13 @@
+# statement عبارة عن سطر كود ينفذ حاجة معينة
+name = "ahmed"
+
+# expression او أخزن الشي ده في متغير لو نعم يبقا  print أسال نفسك ديما هل أقدر اعمل 
+# expression "Hello" - قيمة أقدر اخزنها 5.5 - 42 
+
+# if statement but 12 > 45 is a Boolean expression
+# Boolean expression 12 > 45 لكن if statement السطر ده كلو 
+#if 12 > 45:
+
+# None وقيمة None is a None type 
+print(type(None))
+# None (return)بترجع functions يوجد 
