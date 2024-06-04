cd75859d1bf474e41c238d62279323de14847a06
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-04 08:43:01 +0300

    تنظيم

---
+++ Section9/list1.py
@@ -0,0 +1,14 @@
+# list >> [] [save any data type in list ]
+# nested list [[[]]]
+
+list_of_value = [
+    ['ahmed' , 'Free Palstina' , 2024],
+    [1 , 3 , 5],
+    ['Hello',["I wake up at 5 AM"]]
+] 
+print(list_of_value)
+print("-"*20)
+
+# call function list() convert any data type to save in list []
+convert_data_to_list = list(('ahmed',3.5 , 4 , True , False , None))
+print(convert_data_to_list)
