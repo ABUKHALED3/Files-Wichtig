2fc4684e703ad2fb030f5d4e65e4e0195959d10c
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-06 08:34:08 +0300

    new lesson

---
+++ Section9/index3.py
@@ -0,0 +1,9 @@
+# any data types عند تغير قيمة واحده فقط أقدر احط مكانها 
+# a list 
+names = ['Ahmed', 'Hesham', "Mohamed", "EL Sayed"]
+names[3] = 1
+print(names)
+
+# itreable>> list [] - tupple() - str لازم تبقا  slic لو التغير هيكون في 
+names [1:3] = ['Cairo']
+print(names)
