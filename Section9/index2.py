68d2a42bf01147869362deaf2db43cdfc14305fc
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-06 06:58:01 +0300

    trianing

---
+++ Section9/index2.py
@@ -0,0 +1,19 @@
+#exclusive ليس منضم معي 
+#inclusive منضم معي
+#slicing تقطيع
+
+# A list of names 
+names = ['Ahmed', 'Amr', 'Hesham', 'KhaLed', 'Zeyad']
+
+#هبدأ تقطيع من القيمة صفر لحد 2 و قيمة 2 مش هتكون معايا مهم دي 
+print(names[0:2])
+
+# طريقة أخري
+# أنهي عند القيمة الخامس عشان احط القيمة الأخيرة اللي عندي
+print(names[:5])
+
+# هبدا من القيمة 0 
+print(names[0:])
+
+# start at 0 and end in -1
+print(names[:-1])
