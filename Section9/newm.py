03c2071b58a7970762116f9f646997a54b5173fb
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-20 17:48:10 +0300

    درس جديد قبل ما النور يقطع

---
+++ Section9/newm.py
@@ -0,0 +1,19 @@
+# mehtod append und extend
+
+cars_germany = ['Audi', 'Seat', 'VolksWagen', 'BMW', 'Mercedes-Benz','Opel','Porsche']
+
+cars_japan = ['Toyota', 'Mazda','Honda','Nissan','Mitsubishi']
+
+# append method .append() add list,tuple and str into list or any itrable
+name = ['ahmed']
+cars_germany.append(name)
+print(cars_germany)
+
+# extend method .etend() add value into list or any itrable
+
+nums = [1, 2.5]
+
+cars_germany.extend(nums)
+print(cars_germany)
+
+
