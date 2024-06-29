e539027f8c5d880dc50eed3f6899d7aa9a7d27d4
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-29 15:34:47 +0300

    درس مهم جدا

---
+++ Section10/loop1.py
@@ -0,0 +1,12 @@
+# infinte loop use while loop 
+# False ويكون  boolean expression عشان اتجنب هذا لازم يحصل تغير في 
+#num1 = 50
+#while num1 <=50:
+#    print('You Won')
+
+# print numbers 0 to 10 use while loop
+# num 11 لحد ما يوصل
+num = 0
+while num <=10:
+    print(num)
+    num += 1
