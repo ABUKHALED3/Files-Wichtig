529aeb2822379e39859cefbef79f39f6c6dae065
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-29 16:31:43 +0300

    مهم

---
+++ Section10/Projects/loop5.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'banana', 'orange', 'grape', 
+'kiwi', 'mango', 'strawberry', 'pineapple']
+
+index= 0
+
+# use a while loop to print every item in list
+# boolean expersion False when index >> 8
+while index < len(fruits):
+    # print the current value item
+    print(fruits[index])
+    # زود واحد
+    index += 1
