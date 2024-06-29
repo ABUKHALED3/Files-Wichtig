3f94d4cf45bc0da5fa0ae31f7d9b4909f78183ad
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-06-29 16:00:18 +0300

    تدريبات

---
+++ Section10/Projects/loop1.py
@@ -0,0 +1,11 @@
+# print numbers 1 to 100
+
+num = 1 
+
+# use while loop
+# boolean expersion False when num = 101
+while num <= 100:
+    print(num)
+    
+    #زود علي القيمة 1  
+    num += 1
---
+++ Section10/Projects/loop2.py
@@ -0,0 +1,11 @@
+# print even number 2 >>> 1000
+ 
+even_num = 2
+
+# use while loop 
+# boolean expersion False when even num >>> 1002
+while even_num <= 1000:
+    print(even_num)
+
+    # زود علي القيمة 2
+    even_num += 2
