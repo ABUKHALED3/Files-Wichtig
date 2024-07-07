50f0e4c81ba3779907feadc9822f15c99785e72e
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-07 16:37:55 +0300

    training1

---
+++ Section10/Projects/loop6.py
@@ -0,0 +1,9 @@
+# print nummer zwischen 0 >>> 100
+
+num = 0
+# أقل من او يساوي 100 num طول ما الـ 
+# end when num = 101 >>> False
+while num <= 100:
+    print('Die Nummer ist',num)
+    # num by 1 بزود الـ 
+    num += 1
---
+++ Section10/Projects/loop7.py
@@ -0,0 +1,13 @@
+# Print the total of numbers zwischen 70 to 160. 
+
+num = 70
+total = 0
+
+while num <= 160:
+    # num مع total اجمع الـ 
+    total = total + num
+    # num 1 زود الـ 
+    num += 1
+    
+# أطبع المجموع كلو
+print('Die Total ist',total)
