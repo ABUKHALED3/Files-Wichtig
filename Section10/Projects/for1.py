f025bbb5a2d4b574f93866fa11014ae24e166217
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-16 14:33:19 +0300

    training1

---
+++ Section10/Projects/for1.py
@@ -0,0 +1,15 @@
+# find the total of the numbers in the list use for loop
+
+# Initialize a variable to save into the sum numbers
+total = 0
+
+# a list of numbers 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# use for loop 
+for number in numbers:
+    total += number
+    
+
+# print the total of the numbers in the list
+print(total)
