ca5d11db460f03bac9930582c56f68bff8421e6c
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-16 14:44:18 +0300

    training2

---
+++ Section10/Projects/for2.py
@@ -0,0 +1,11 @@
+# a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# use for loop 
+for num in numbers:
+    # num اوس الرقم اللي في square القيمة اللي جوه 
+    # 1**2 
+    square = num ** 2
+
+    # print the square of each number
+    print(f'The square of {num} is {square}')
---
+++ Section10/Projects/for3.py
@@ -0,0 +1,8 @@
+#  Print the characters of the following string, each on a line. 
+
+string = 'Codezilla Python Course'
+
+# use a for loop 
+for char in string:
+    # print each character
+    print(char)
