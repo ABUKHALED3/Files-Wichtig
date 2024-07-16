07833cbab7f86e9098ec0afb3ddf06321f1fc0f2
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-16 14:20:23 +0300

    for loop

---
+++ Section10/for1.py
@@ -0,0 +1,16 @@
+# lesson about for loop 
+
+# a list of numbers 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# use for loop 
+# numbers  يساوي القيمة اللي جو  number المتغير 
+for number in numbers:
+    print(number)
+
+names = ['Ahmed', 'Amr', 'Amir', 'Amgad']
+
+#name و خزن كل قيمة في الـ  names بقولو لف جو الـ 
+for name in names: 
+    # print valaue in name
+    print(name)
