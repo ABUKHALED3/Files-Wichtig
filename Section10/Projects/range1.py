67a34bfe83ccad2ecc3d57ba43a4151bd364d7ff
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-17 16:20:54 +0300

    تدريبات بسيطه بس حلوه

---
+++ Section10/Projects/range1.py
@@ -0,0 +1,13 @@
+# print a list of odd numbers from 1 to 100
+
+# create a variable to save into the odd Numbers from 1 to 100  
+# use list() to added it in list
+odd_numbers = list(range(1, 101, 3))
+print(odd_numbers)
+
+# print  a list of even numbers from 8 to 422
+
+# create a variable to save into the odd Numbers from 8 to 422
+# use list() to added it in list
+even_numbers = list(range(8, 423, 2))
+print(even_numbers)
