492751a66917d60192cc52455754f2707637b91c
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-17 16:27:51 +0300

    اخر تدريب حلوه كده

---
+++ Section10/Projects/range2.py
@@ -0,0 +1,19 @@
+# print a list of numbers from 120 to 450, increasing by 7 
+
+# create a variabel to save into this numbers  وهزود 7 كل مره
+# use list() to save numbers in list
+numbers = list(range(120, 451, 7))
+print(numbers)
+
+print('-'*60)
+
+# print the sum of even numbers from 28 to 228 
+
+# create a variable to save into even numbers from 28 to 228
+# use list function to add this numbers in list
+
+even_numbers = list(range(28, 229, 2))
+
+# use function sum() to sum this numbers
+total = sum(even_numbers)
+print(total)
