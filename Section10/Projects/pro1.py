d895276ce5bf0e0b1d3ecc76dc033f50ae7d7c54
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-17 17:18:45 +0300

    training1

---
+++ Section10/Projects/pro1.py
@@ -0,0 +1,13 @@
+# print the total numbers between 0 to 100
+
+# create a variable to save into the total numbers
+total = 0
+
+# use for loop 
+# use range()
+for i in range(101):
+    #total علي i زود قيمة  
+    total+=i
+
+# print the total numbers
+print(f'The Total numbers between 0 to 100 is {total}')
---
+++ Section10/Projects/pro2.py
@@ -0,0 +1,11 @@
+# Print the total of numbers between 70 to 160. 
+
+# create a variable to save into the total numbers between 70 to 160
+total = 0
+
+# use for loop and use function range() 
+for i in range(70, 161):
+    total += i
+
+# print the total
+print(f'The Total numbers between 70 to 160 is {total}')
