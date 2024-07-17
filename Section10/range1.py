f619ee9cec7e8ab84d54476336d76364f4959a53
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-17 16:12:20 +0300

    درس جديد و بسيط

---
+++ Section10/range1.py
@@ -0,0 +1,13 @@
+# lesson about function range()
+
+# strat 0 by default to 6 because stop 7 not included and زود 1 
+numbers = range(7)
+for num in numbers:
+    print(num) 
+
+# هبدا من صفر
+# هزود كل مرة 2 
+# هنتهي عند 20
+# list حط الأرقام اللي هتطلع دي في 
+even_numbers = list(range(0,21,2))
+print(even_numbers)
