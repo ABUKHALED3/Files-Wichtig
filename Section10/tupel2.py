f650a8b775638da6f3e1f24ccb3196bdaf14d9a3
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-07-22 20:01:32 +0300

    درس جديد 2

---
+++ Section10/tupel2.py
@@ -0,0 +1,18 @@
+# index in Tupels like list 
+numbers = (1, 5, 'A')
+print(numbers[-1]) # >> A
+
+# Slicing in Tupels like list
+print(numbers[ :2])
+
+print('-'*50)
+
+# Tupels ist imutable غير قابلة لتعديل و الحذف وكل شي
+
+# الفرق بين List and Tupels
+# list ist mutable قابلة لتعديل و الحذف وكل شي
+numbers_lst = [1, 4, 5, 'Number']
+numbers_lst [-1] = 'ahmed'
+print(numbers_lst)
+
+
