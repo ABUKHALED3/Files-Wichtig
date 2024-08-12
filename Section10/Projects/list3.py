4468f46d4bb31903c76804d96a01c69ee58ee432
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-12 20:25:30 +0300

    traning2

---
+++ Section10/Projects/list3.py
@@ -0,0 +1,8 @@
+scores = [75, 87, 93, 98, 82, 67, 91, 88] 
+
+# create a list of precent
+#list scores لف جو 
+#num ثم حفظ كل قيمة في متفير اسمو 
+#num to str  ثم تحويل قيمة الـ 
+precent = [str(num) +'%' for num in scores]
+print(precent)
---
+++ Section10/Projects/list4.py
@@ -0,0 +1,4 @@
+fruits = ["APPLE", "ORANGE", "BANANA", "PEAR", "MANGO"] 
+
+fruits_lower = [fruit.lower() for fruit in fruits]
+print(fruits_lower)
