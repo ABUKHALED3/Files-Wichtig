6c1a02164383dc48f4db1e822969191b4c1e0231
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-12 21:00:48 +0300

    training 6

---
+++ Section10/Projects/list9.py
@@ -0,0 +1,21 @@
+# list of words 
+words = [ 
+        'have', 'that', 'they', 'with', 'this', 'from', 
+'which', 'would', 'will', 'there', 
+        'make', 'when', 'more', 'other', 'what', 'time', 
+'about', 'than', 'into', 'could',  
+        'state', 'only', 'year', 'some', 'take', 'come', 
+'these', 'know', 'like', 'then',  
+        'first', 'work', 'such', 'give', 'over', 'think', 
+'most', 'even', 'find', 'also',  
+        'after', 'many', 'must', 'look', 'before', 'great', 
+'back', 'through', 'long',  
+        'where', 'much', 'should', 'well', 'people', 'gouda', 
+'just', 'because', 'good',  
+        'each', 'those', 'feel', 'seem', 'high', 'place', 
+'little', 'world', 'very', 'still',  
+        'nation', 'hand', 'life', 'tell', 'write', 'become' 
+    ] 
+
+upper_word = [word.upper() for word in words]
+print(upper_word)
