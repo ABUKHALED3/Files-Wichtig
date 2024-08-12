7f26516d14cab574839b3ff2a87cc1344342f67c
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-12 20:18:50 +0300

    trainig1

---
+++ Section10/Projects/list1.py
@@ -0,0 +1,8 @@
+nums = [2, 29, 4, 8, 42, 55, 70, 74, 78, 27] 
+
+# create a list of negative nums
+#list nums لف جو الـ 
+#num احفظ كل قيمة فيها في متغير اسمو 
+# ثم ضيف القيمة دي بي السالب
+negative_nums = [-num for num in nums]
+print(negative_nums) 
---
+++ Section10/Projects/list2.py
@@ -0,0 +1,7 @@
+nums = [2, 29, 4, 8, 42, 55, 70, 74, 78, 27] 
+
+#list nums هلف جو الـ 
+#num ثم هخزن كل قيمة في المتغير اسمو 
+#num ثم هضرب قيمة  اس اتنين
+squares = [num**2 for num in nums]
+print(squares)
