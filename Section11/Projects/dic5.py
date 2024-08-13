5f2e9ec6028bfd7de5bfad8212b62620338de358
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 19:25:15 +0300

    Training

---
+++ Section11/Projects/dic5.py
@@ -0,0 +1,11 @@
+
+# create a dictionary grades 
+grades = {
+    ('Ahmed', 'Hassan') :   87,
+    ('Hossam', 'Ali') :     90,
+    ('Mohamed', 'Kamel'):   100,
+    ('Hesham', 'Zezo') :    74,
+    ('Ahmed', 'Khaled'):    95
+} 
+
+print(grades['Ahmed','Khaled'])
