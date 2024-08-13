a21acea5f3df57668f1bd98d9925cda88a8a6841
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 19:14:35 +0300

    Training

---
+++ Section11/Projects/dic3.py
@@ -0,0 +1,16 @@
+# Create a dictionary called drinks that contains the following 
+
+# Coke - 10 
+# Sprite - 8 
+# Fanta - 8 
+# Pepsi – 10 
+
+# Create a dictionary drinks 
+drinks = {
+    'Coke':   10,
+    'Sprite': 8,
+    'Fanta':  8,
+    'Pepsi':  10
+}
+
+print(drinks)
