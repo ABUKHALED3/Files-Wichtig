36f8d4ba7902a184f2b8bdb8c5796f5ef20615b4
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 19:30:21 +0300

    Training

---
+++ Section11/Projects/dic6.py
@@ -0,0 +1,15 @@
+# Hawaiian Pizza 
+pizzas = { 
+# Key >>>>    Value    
+"Margherita": 100,  
+"Pepperoni": 120,  
+"Meat Lovers": 150,  
+"Chicken": 130, 
+"Cheese": 100, 
+"Veggie": 120, 
+"Hawaiian": 150, 
+} 
+
+print(f'Price Pepperoni: {pizzas["Pepperoni"]} EGP')
+print(f'Price Chicken: {pizzas["Chicken"]} EGP')
+print(f'Price Hawaiian: {pizzas['Hawaiian']} EGP')
