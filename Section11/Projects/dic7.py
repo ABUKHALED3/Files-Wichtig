6c011ff7eafb6d80b1698cac16dcdf02b33879dd
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 19:34:51 +0300

    Training

---
+++ Section11/Projects/dic7.py
@@ -0,0 +1,18 @@
+#   Coke  - Mango  Tea -  Coffee
+
+drinks = { 
+# Key >> Value    
+"Coke": 30, 
+"Sprite": 25, 
+"Fanta": 25, 
+"Pepsi": 30, 
+"Tea": 20, 
+"Coffee": 25, 
+"Orange Juice": 30, 
+"Mango Juice": 30 
+} 
+
+print(f'Coke Price: {drinks['Coke']}EGP')
+print(f'Mango Juice Price: {drinks['Mango Juice']}EGP')
+print(f'Tea Price: {drinks['Tea']}EGP')
+print(f'Coffee Price: {drinks['Coffee']}EGP')
