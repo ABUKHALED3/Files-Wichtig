fa7bc5cf76e37475d3fc3f1589b993d5190893e3
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 19:11:45 +0300

    Training

---
+++ Section11/Projects/dic2.py
@@ -0,0 +1,17 @@
+#2. Create a dictionary called students that contains the 
+#following information: 
+
+# Mohamed Hassan - 83 
+# Ahmed Khaled - 97 
+# Ali Hamed - 75 
+# Mahmoud Samir - 89 
+
+# create a dictionary students
+students = {
+    'Mohamed Hassan': 83,
+    'Ahmed Khaled' : 97,
+    'Ali Hamed' : 75,
+    'Mahmoud Samir': 89
+}
+
+print(students['Ahmed Khaled'])
