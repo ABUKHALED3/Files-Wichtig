5b869ef5227b23fee090c09f6700f494a29e3f04
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 20:27:46 +0300

    Training

---
+++ Section11/Projects/dic8.py
@@ -0,0 +1,23 @@
+
+menu = { 
+        # Key >>>>  Value
+        "Cheese pizza": 100, 
+        "Veggie pizza": 120, 
+        "Hawaiian pizza": 150, 
+        "Coke": 30, 
+        "Sprite": 25, 
+        "Fanta": 25, 
+}
+
+# أضافة
+# key >>>       Value
+menu['Ice Cream'] = 30
+menu['Chocolade Cake'] = 70
+menu['Cheese Cake'] = 70
+menu['Brownie'] = 40
+menu['Donut'] = 30
+
+#Value تغير الـ 
+menu['Hawaiian pizza'] = 190
+
+print(menu)
