364a649b2a5529f96b39e42cd650944033c6756a
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 20:34:41 +0300

    Training

---
+++ Section11/Projects/dic9.py
@@ -0,0 +1,26 @@
+# a Dictionary
+drinks = { 
+# Key>>> Value    
+"Latte": 30, 
+"Coke": 30, 
+"Sprite": 25, 
+"Fanta": 25, 
+"Pepsi": 30, 
+"Tea": 20, 
+"Coffee": 25, 
+"Orange Juice": 30, 
+"Mango Juice": 30 
+} 
+
+# print the prices Soda Items und Hot Drinks
+print('Hot Drinks:')
+print(f'Latte Price: {drinks['Latte']}EGP')
+print(f'Tea Price: {drinks['Tea']}EGP')
+print(f'Coffee Price: {drinks['Coffee']}EGP')
+
+print('-'*30)
+print('Soda Items')
+print(f'Coke Price: {drinks['Coke']}EGP')
+print(f'Sprite Price: {drinks['Sprite']}EGP')
+print(f'Fanta Price: {drinks['Fanta']}EGP')
+print(f'Pepsi Price: {drinks['Pepsi']}EGP')
