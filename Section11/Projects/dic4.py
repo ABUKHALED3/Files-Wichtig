7405de3dcdcee06eb7e7529bb6a05ee331432495
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-08-13 19:18:33 +0300

    Training

---
+++ Section11/Projects/dic4.py
@@ -0,0 +1,10 @@
+codezilla_students = {
+    #Key >>> Value
+    1: 'Mohamed Ahmed',
+    2: 'Ahmed Khaled',
+    3: 'Ali Hamed',
+    6: 'Mahmoud Samir',
+    8: 'Ahmed Hassan'
+}
+
+print(codezilla_students[1])
