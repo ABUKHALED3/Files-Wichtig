143261d9a2836692507ec8716bba14f53e5f7c2a
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-14 21:01:03 +0300

    New lesson

---
+++ Section12/fun1.py
@@ -0,0 +1,17 @@
+# new lesson intro about function 
+
+# a list numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+#عشان اجمع الأرقام function sum() استخدام 
+print(sum(numbers))
+
+total = 0
+# for loop استخدام 
+for number in numbers:
+    total += number
+
+print(total) 
+
+#مهم جدا  functions ا شايف الفرق كبير جدا ازي فا استخدام الـ 
+#بدون ما اعرف بتشتغل ازي ادما اعطتني النتيجة اللي انا عايزها يبقا تمام  functions  بستخدم الـ 
