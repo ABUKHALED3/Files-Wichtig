4dbe902d3dcf840b3168d4c80ba454f8a11cbecc
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-15 17:52:50 +0300

    chanage simple

--- Section12/fun2.py
+++ Section12/fun2.py
@@ -10,8 +10,8 @@ def greet_user(name, age):
     print(f"Welcome to Codezilla {name} your age is {age}")
 
 # get from user name und age
-name = input('Enter your name: ')
-age = input('Enter your age: ')
+name_user = input('Enter your name: ')
+age_user = input('Enter your age: ')
 
 # call to function
-greet_user(name, age)
+greet_user(name_user, age_user)
