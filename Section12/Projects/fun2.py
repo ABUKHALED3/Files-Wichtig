25ac5ae19e5887b61c1f3e099cdecd1565b9b666
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-15 18:08:45 +0300

    training

---
+++ Section12/Projects/fun2.py
@@ -0,0 +1,10 @@
+def text_reverse(txt):
+    # sclicing 
+    # اعكس اللي هيكتب
+    print(txt[::-1])
+
+# get from user txt
+txt_user = input('Enter your text: ')
+
+# call text_reverse
+text_reverse(txt_user)
