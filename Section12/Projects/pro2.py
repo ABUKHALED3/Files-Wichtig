4b44792b366ce9609f550642c487d289c195158a
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-16 15:16:04 +0300

    Fix bugs

---
+++ Section12/Projects/pro2.py
@@ -0,0 +1,19 @@
+def employee_info(name, age, salary):
+    print(f"Name: {name.title()}") 
+    print(f"Age: {age}") 
+    print(f"Salary: {salary}\n") 
+ 
+ 
+employee_info("Mohamed ahmed", 25, 10_000) 
+
+employee_info(age = 33, salary=15_000, name="hassan Ali") 
+
+employee_info(salary=20_000, age=30, name="Ali Hassan") 
+
+employee_info("Ahmed Mohamed", 35, 15_000) 
+
+employee_info("Hazem Khaled", 25, salary=15_000) 
+
+employee_info('Hamed Ali', 35, 14_000) 
+
+employee_info("Mohamed khedr", salary=13_000, age=25,) 
