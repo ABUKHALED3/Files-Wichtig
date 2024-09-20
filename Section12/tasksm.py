7808183ec4991e0947878200148fb93fcc167a95
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-20 18:59:35 +0300

    Task Manger vierte

--- Section12/tasksm.py
+++ Section12/tasksm.py
@@ -1,12 +1,41 @@
+# global variables
+tasks = []
+completed_tasks = []
 
-message = """\n1- Add Tasks to a list 
+#هحط جوها الكود الأساسي اللي تحت ده بدل ما هو سايح كده علي بعضه ويلغبط  function  هعمل 
+# وزي ما حضرتك شايف الكود بقا كده منظم جدا وعشان نقدر نكتشف الاخطاء لو في خطا
+
+def main():
+
+    message = """\n1- Add Tasks to a list 
 2- Mark Tasks as complete
 3- View Tasks
 4- Completed Tasks
 5- Quit\n"""
+    # use while loop
+    while True:
+        print(message)
 
-tasks = []
-completed_tasks = []
+        # get from user choice
+        choice = input('Enter your Choice: ')
+
+        if choice == '1':
+            add_task()
+
+        elif choice == '2':
+            mark_task_complete()
+
+        elif choice == '3':
+            view_tasks()
+
+        elif choice == '4':
+            view_completed_tasks()
+        
+        elif choice == '5':
+            break
+        
+        else:
+            print('Invalid Choice, Please enter the number between 1 and 4!\n')
 
 def add_task():
     # get from user task
@@ -37,7 +66,7 @@ def mark_task_complete():
     incomplete_tasks[task_number - 1]['Completed'] = True
 
     print('Task marked completed🥳')
-    
+    completed_tasks.append(incomplete_tasks[task_number - 1])
     
 
 def view_tasks():
@@ -54,33 +83,11 @@ def view_tasks():
 def view_completed_tasks():
     if completed_tasks:
         for i , task in enumerate(completed_tasks,1 ):
-            print(f'{i}- {task}\n🥳')
+            for value in task.values():
+                if value is not True:
+                    print(f'{i} {value} 🥳')
     else:
         print('Completed Tasks is Empty🤔')
 
 
-# use while loop
-while True:
-    print(message)
-
-    # get from user choice
-    choice = input('Enter your Choice: ')
-
-    if choice == '1':
-        add_task()
-
-    elif choice == '2':
-        mark_task_complete()
-
-    elif choice == '3':
-        view_tasks()
-
-    elif choice == '4':
-        view_completed_tasks()
-    
-    elif choice == '5':
-        break
-    
-    else:
-        print('Invalid Choice, Please enter the number between 1 and 4!\n')
-
+main()
