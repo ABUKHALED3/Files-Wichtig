05c460429c164cbb14d1e455631f3fe1f199dff0
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-25 20:12:25 +0300

    هنكمل بكره

---
+++ Section13/Projects/books_summaries/all_books.py
@@ -0,0 +1,17 @@
+books =  {'Buch1': 'atomic_habits.txt',
+          'Buch2': 'deep_work.txt',
+          'Buch3': 'dopamine_nation.txt',
+          'Buch4': 'power_habit.txt',
+          'Buch5': "so_good_they_can't.txt",
+          'Buch6': 'tasting_tee.txt'
+          }
+
+# read all books und add all txt in file 
+
+def read_book (file_name):
+
+    with open(file_name) as f:
+        # read the book 
+        data = f.read()
+        return data
+    
