f512b241fadf88a88a35a47e12ad8a853d9d5b94
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-25 18:48:10 +0300

    Drittel Projects

--- Section13/Projects/projects2.py
+++ Section13/Projects/projects2.py
@@ -1,10 +1,14 @@
-# open file Deep Work in mode read as f
-# open or create new deep work in mode write as fout
-with open('deep_work.txt') as f, open('new_deep_work','w') as fout:
-    # read the file deep work und conver to upper und reverse
-    data = f.read().upper() [ : : -1] 
+#عشان استخدمه علطول function ريحت الدماغ وعملت الكود جوه 
+def file_red_rev (file_name):
     
-    # fout write in file this data
-    fout.write(data)
+    # open any file in mode read as f
+    # open or create new files in mode write as fout
+    with open(file_name) as f, open(f'new_{file_name}','w') as fout:
+        
+        # read the file  und convert to upper und reverse
+        data = f.read().upper() [ : : -1] 
+    
+        # fout write in file this data
+        fout.write(data)
 
-print(fout)
+file_red_rev('deep_work.txt')
