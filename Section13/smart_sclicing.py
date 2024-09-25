2b9c9bfa2b98ab14554725d777ae5f4f7fe46070
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-25 15:59:45 +0300

    Auch Nue Lektion

---
+++ Section13/smart_sclicing.py
@@ -0,0 +1,30 @@
+# list of Nummer
+nums = [10 ,20 ,30 ,40 ,50 ,60 ,70 ,80 ,90 ,100]
+
+# sclicing 
+
+# start -anfangen: stop : step 2 >> default step 1
+print(nums[0::2])
+
+# strat: stop : step
+print(nums[:6:1])
+
+# strat: stop: step 3
+print(nums[::3])
+
+print('-'*30)
+
+# step by negative - بالسب الرجوع الي الشمال
+# start: stop: step
+#list  بعكس الـ 
+print(nums[::-1])
+
+# start: stop: step -4 
+# 4  الي الشمال
+print(nums[::-4])
+
+print('-'*30)
+
+txt = 'Ahmed KhaLed'
+
+print(txt[::-1])
