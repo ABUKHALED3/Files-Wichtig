26a9713efd4f6d2bcc8bce56af55d2114da7a2da
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-09-26 20:56:54 +0300

    neue lektion

---
+++ Section13/intro_regex.py
@@ -0,0 +1,34 @@
+# intro to Regex 
+
+
+txt = """Name: Hamada Codezilla
+
+Email: hamada23.codezilla@codezilla.com
+
+Phone: 01743507569
+
+Address: 456 Nile St, Giza, Egypt
+
+DOB: 28/02/1985
+
+Hamada Codezilla is a software engineer with over 10 years of
+
+experience in the industry. He is skilled in web development,
+
+databases, and programming languages such as Python and
+
+JavaScript. He has worked for a variety of companies, from
+
+small startups to large corporations.
+
+Hamada can be reached at hamada23.codezilla@codezilla.com or
+
+hamada_codezilla35@gmail.com or by phone at 01743507569. His
+
+address is 456 Nile St, Giza, Egypt. He was born on February
+
+28, 1985.
+
+In his free time, Hamada enjoys playing Ping-Pong, reading tech
+
+blogs, and spending time with his family."""
