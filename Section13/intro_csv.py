be15832d5fba39b5ac77a3b6798800c1922de4d4
Ahmed KhaLed <mr.ahmed.khaled@hotmail.com>
2024-10-01 16:50:31 +0300

    دماغي فرقعت

--- Section13/intro_csv.py
+++ Section13/intro_csv.py
@@ -1,7 +1,7 @@
 # import library csv
 import csv
 
-# create a function to read csv file
+# def a function to read csv file
 def read_csv_file(file_name):
     """ Get file name
         Read the data the from file 
@@ -20,7 +20,7 @@ def read_csv_file(file_name):
 
         return file_data_lst
     
-# Create a function to  Double the salary of each Employee 
+# def a function to  Double the salary of each Employee 
 def update_salary(employees_lst):
 
     # use for loop through csv_data_lst
@@ -36,17 +36,29 @@ def update_salary(employees_lst):
     
     return employees_lst
 
+# def function 
+def employess_salary (salary):
+    # رتب علي اساس
+    # salary [-1] الاول
+    #شبه بعضه salary في تطابق و الـ 
+
+    #salary[0] رتب علي اساس الـ 
+    # في تطابق بردو 
+
+    #salary[1] رتب علي اساس الـ 
+    return  float(salary [-1]) , salary[0] , salary[1]
+
 # call to function read csv file und save it data in a file
 csv_data_lst = read_csv_file('employees_data.csv')
 
 # call to function update salary
-updated_eployees = update_salary(csv_data_lst)
+updated_employees = update_salary(csv_data_lst)
 
-
-# Sort employees by salary
-sorted_employess = sorted(updated_eployees)
+sorted_employess = sorted (updated_employees, key= employess_salary)
 
 for emp in sorted_employess:
     print(emp)
+
+
 
 # Write employess data to a file
